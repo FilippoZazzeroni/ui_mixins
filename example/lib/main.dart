3c510e4df7c966ae1a00fd8cb64e280fdb33feb8
mostafa <mostafa.saad.emara@gmail.com>
2022-03-07 14:30:32 +0200

    implment transitionable in awidget

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:ui_mixins/mixins/loadble/dialog_type.dart';
 import 'package:ui_mixins/mixins/loadble/loadble.dart';
 import 'package:ui_mixins/mixins/loadble/loading_data.dart';
+import 'package:ui_mixins/mixins/sizeble.dart';
+import 'package:ui_mixins/mixins/transitionable.dart';
 
 void main() {
   runApp(const MaterialApp(home: MainView()));
@@ -46,10 +48,46 @@ class MainView extends StatelessWidget with Loadable {
                   await Future.delayed(const Duration(seconds: 3));
                   Navigator.pop(context);
                 },
-                child: const Text("loadable")),
+                child: AnimatedWidget()),
           )
         ],
       ),
     );
+  }
+}
+
+class AnimatedWidget extends StatefulWidget {
+  const AnimatedWidget({Key? key}) : super(key: key);
+
+  @override
+  State<AnimatedWidget> createState() => _AnimatedWidgetState();
+}
+
+class _AnimatedWidgetState extends State<AnimatedWidget>
+    with TickerProviderStateMixin, Transitionable, Sizeable {
+  late AnimationController _controller;
+
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 3),
+      vsync: this,
+    )..forward();
+    _animation = Tween<double>(begin: 1, end: 60).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInCubic,
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return transitionBuilder(
+        context,
+        _animation,
+        Container(
+            color: Colors.red, width: width(context), height: height(context)));
   }
 }
--- lib/mixins/transitionable.dart
+++ lib/mixins/transitionable.dart
@@ -3,12 +3,11 @@ library ui_mixins;
 import 'package:flutter/cupertino.dart';
 
 mixin Transitionable {
-  Widget transitionBuilder(BuildContext context, Animation<double> animation, Animation _, Widget child) {
-
-
+  Widget transitionBuilder(
+      BuildContext context, Animation<double> animation, Widget child) {
     return SlideTransition(
-          position: Tween(begin: const Offset(0, 1), end: const Offset(0, 0))
-              .animate(animation),
-          child: child);
+        position: Tween(begin: const Offset(0, 1), end: const Offset(0, 0))
+            .animate(animation),
+        child: child);
   }
 }
