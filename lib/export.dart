ad1294f926f8d71511a970b7fc941be0c8b3717f
FilippoZazzeroni <55580351+FilippoZazzeroni@users.noreply.github.com>
2022-04-12 09:46:04 +0200

    Added_export_dart

--- lib/export.dart
+++ lib/export.dart
@@ -2,3 +2,6 @@ library ui_mixins;
 
 export 'package:ui_mixins/mixins/serializable.dart';
 export 'package:ui_mixins/mixins/sizeble.dart';
+export 'package:ui_mixins/mixins/navigable/navigable.dart';
+export 'package:ui_mixins/mixins/navigable/route_scope.dart';
+export 'package:ui_mixins/mixins/navigable/router.dart';
